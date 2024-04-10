373c1e130dfb09fce6fb6739743be8ad9ba99f25
wuwei <wuwei@fadada.com>
2024-04-10 14:21:52 +0800

    fix: 修改路由结构

--- src/client/router/Layout/index.vue
+++ src/client/router/Layout/index.vue
@@ -4,9 +4,10 @@
       <div class="logo">神族九帝</div>
       <a-menu
         v-model:selectedKeys="currentKey"
-        mode="vertical"
+        mode="inline"
         theme="dark"
         :items="items"
+        @click="handleClick"
       />
     </a-layout-sider>
     <a-layout-content class="content">
@@ -18,16 +19,35 @@
 <script setup>
 import { ref, h } from "vue";
 import { IeOutlined } from "@ant-design/icons-vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
 
 const items = ref([
   {
-    key: "puppeteer",
+    key: "/puppeteer",
     icon: () => h(IeOutlined),
     label: "无头浏览器",
-    title: "Navigation One",
+    title: "无头浏览器",
+    children: [
+      {
+        key: "/puppeteer/screenshot",
+        label: "截图",
+        title: "截图",
+      },
+      {
+        key: "/puppeteer/pdf",
+        label: "pdf",
+        title: "pdf",
+      },
+    ],
   },
 ]);
 const currentKey = ref(["puppeteer"]);
+
+const handleClick = ({ key }) => {
+  router.push(key);
+};
 </script>
 
 <style lang="scss" scoped>
--- src/client/router/index.js
+++ src/client/router/index.js
@@ -5,12 +5,22 @@ export const router = createRouter({
   routes: [
     {
       path: "/",
-      redirect: "/screenshot",
       component: () => import("./Layout/index.vue"),
+      redirect: "/puppeteer",
       children: [
         {
-          path: "/screenshot",
-          component: () => import("@/views/screenshot/index.vue"),
+          path: "/puppeteer",
+          redirect: "/puppeteer/screenshot",
+          children: [
+            {
+              path: "screenshot",
+              component: () => import("@/views/puppeteer/screenshot/index.vue"),
+            },
+            {
+              path: "pdf",
+              component: () => import("@/views/puppeteer/pdf/index.vue"),
+            },
+          ],
         },
       ],
     },
