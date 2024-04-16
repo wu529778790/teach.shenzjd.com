031ab74a1b5eb2e24d357ae18199d3ada8d9b519
wuwei <wuwei@fadada.com>
2024-04-16 20:46:05 +0800

    feat: 增加全局日志

--- src/server/main.js
+++ src/server/main.js
@@ -8,6 +8,12 @@ const router = express.Router();
 // 解析此应用程序的 JSON 正文。确保在路由处理程序之前放置 app.use(express.json())
 app.use(express.json());
 
+// 全局日志
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+})
+
 // 添加/api前缀
 app.use("/api", router);
 
