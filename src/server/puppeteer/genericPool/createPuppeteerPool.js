bb7973bd5f08990764ac68ce0d5cec0ba54a96d7
wu529778790 <529778790@qq.com>
2024-04-17 22:46:02 +0800

    fix: 调整generic-pool

--- src/server/puppeteer/genericPool/createPuppeteerPool.js
+++ src/server/puppeteer/genericPool/createPuppeteerPool.js
@@ -2,86 +2,86 @@ import puppeteer from "puppeteer";
 import genericPool from "generic-pool";
 
 /**
- * Creates a Puppeteer pool with specified configurations.
- *
- * @param {Object} options - Object containing pool configurations.
- * @param {number} options.max - pool 的最大容量
- * @param {number} options.min - pool 的最小容量
- * @param {number} options.idleTimeoutMillis - 连接在池中保持空闲而不被回收的最小时间值
- * @param {number} options.maxUses - 最大使用数
- * @param {boolean} options.testOnBorrow - 在连接池交付实例前是否先经过 factory.validate 测试
- * @param {Object} options.puppeteerArgs - Arguments for launching Puppeteer instances.
- * @param {Function} options.validator - Function to validate the instance.
- * @param {Object} options.otherConfig - Additional configurations.
- * @return {Object} A pool of Puppeteer instances.
+ * 初始化一个 Puppeteer 池
+ * @param {Object} [options={}] 创建池的配置配置
+ * @param {Number} [options.max=10] 最多产生多少个 puppeteer 实例 。如果你设置它，请确保 在引用关闭时调用清理池。 pool.drain().then(()=>pool.clear())
+ * @param {Number} [options.min=1] 保证池中最少有多少个实例存活
+ * @param {Number} [options.maxUses=2048] 每一个 实例 最大可重用次数，超过后将重启实例。0表示不检验
+ * @param {Number} [options.testOnBorrow=2048] 在将 实例 提供给用户之前，池应该验证这些实例。
+ * @param {Boolean} [options.autostart=false] 是不是需要在 池 初始化时 初始化 实例
+ * @param {Number} [options.idleTimeoutMillis=3600000] 如果一个实例 60分钟 都没访问就关掉他
+ * @param {Number} [options.evictionRunIntervalMillis=180000] 每 3分钟 检查一次 实例的访问状态
+ * @param {Object} [options.puppeteerArgs={}] puppeteer.launch 启动的参数
+ * @param {Function} [options.validator=(instance)=>Promise.resolve(true))] 用户自定义校验 参数是 取到的一个实例
+ * @param {Object} [options.otherConfig={}] 剩余的其他参数 // For all opts, see opts at https://github.com/coopernurse/node-pool#createpool
+ * @return {Object} pool
  */
-const createPuppeteerPool = ({
-  max = 10,
-  min = 1,
-  idleTimeoutMillis = 30000,
-  maxUses = 50,
-  testOnBorrow = true,
-  puppeteerArgs = {},
-  validator = () => Promise.resolve(true),
-  ...otherConfig
-} = {}) => {
+export default (options = {}) => {
+  const {
+    max = 10,
+    min = 2,
+    maxUses = 2048,
+    testOnBorrow = true,
+    autostart = false,
+    idleTimeoutMillis = 3600000,
+    evictionRunIntervalMillis = 180000,
+    puppeteerArgs = {},
+    validator = () => Promise.resolve(true),
+    ...otherConfig
+  } = options
+
   const factory = {
-    // 创建实例
     create: () =>
-      puppeteer.launch(puppeteerArgs).then((instance) => {
-        instance.useCount = 0;
-        return instance;
+      puppeteer.launch(puppeteerArgs).then(instance => {
+        // 创建一个 puppeteer 实例 ，并且初始化使用次数为 0
+        instance.useCount = 0
+        return instance
       }),
-    // 销毁实例
-    destroy: (instance) => {
-      instance.close();
+    destroy: instance => {
+      instance.close()
     },
-    // 验证实例可用性
-    validate: (instance) => {
-      return validator(instance).then((valid) =>
-        // maxUses 小于 0 或者 instance 使用计数小于 maxUses 时可用
-        Promise.resolve(valid && (maxUses <= 0 || instance.useCount < maxUses))
-      );
-    },
-  };
+    validate: instance => {
+      // 执行一次自定义校验，并且校验校验 实例已使用次数。 当 返回 reject 时 表示实例不可用
+      return validator(instance).then(valid => Promise.resolve(valid && (maxUses <= 0 || instance.useCount < maxUses)))
+    }
+  }
   const config = {
     max,
     min,
-    idleTimeoutMillis,
     testOnBorrow,
-    ...otherConfig,
-  };
-  // 创建连接池
-  const pool = genericPool.createPool(factory, config);
-  const genericAcquire = pool.acquire.bind(pool);
-  // 池中资源连接时进行的操作
+    autostart,
+    idleTimeoutMillis,
+    evictionRunIntervalMillis,
+    ...otherConfig
+  }
+  const pool = genericPool.createPool(factory, config)
+  const genericAcquire = pool.acquire.bind(pool)
+  // 重写了原有池的消费实例的方法。添加一个实例使用次数的增加
   pool.acquire = () =>
-    genericAcquire().then((instance) => {
-      instance.useCount += 1;
-      return instance;
-    });
-  pool.use = (fn) => {
-    let resource;
+    genericAcquire().then(instance => {
+      instance.useCount += 1
+      return instance
+    })
+  pool.use = fn => {
+    let resource
     return pool
       .acquire()
-      .then((r) => {
-        resource = r;
-        return r;
+      .then(r => {
+        resource = r
+        return resource
       })
       .then(fn)
       .then(
-        (result) => {
-          // 释放资源
-          pool.release(resource);
-          return result;
+        result => {
+          // 不管业务方使用实例成功与后都表示一下实例消费完成
+          pool.release(resource)
+          return result
         },
-        (err) => {
-          pool.release(resource);
-          throw err;
+        err => {
+          pool.release(resource)
+          throw err
         }
-      );
-  };
-  return pool;
-};
-
-export default createPuppeteerPool;
+      )
+  }
+  return pool
+}
--- src/server/puppeteer/genericPool/index.js
+++ src/server/puppeteer/genericPool/index.js
@@ -2,8 +2,10 @@ import createPuppeteerPool from "./createPuppeteerPool.js";
 
 const pool = createPuppeteerPool({
   puppeteerArgs: {
-    // headless: false, // 是否启用无头模式
-    // slowMo: 250, // 慢速模式, 主要是为了看看发生了什么事情
+    ignoreHTTPSErrors: true,
+    // headless: false, // 是否启用无头模式页面
+    timeout: 0,
+    pipe: true, // 不使用 websocket 
   },
 });
 
