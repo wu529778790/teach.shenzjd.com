b75e695c5955ff9feba3903fb74013f2e152d3f5
wuwei <wuwei@fadada.com>
2024-04-18 19:22:16 +0800

    feat: 增加vite-plugin-imagemin配置

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,35 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { visualizer } from 'rollup-plugin-visualizer' // 打包体积分析插件
 import postcssConfig from './postcss.config';
+import viteImagemin from 'vite-plugin-imagemin'; // 图片压缩插件
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    visualizer({ open: false })
+    visualizer({ open: false }),
+    viteImagemin({
+      // 无损压缩配置，无损压缩下图片质量不会变差
+      optipng: {
+        optimizationLevel: 7
+      },
+      // 有损压缩配置，有损压缩下图片质量可能会变差
+      pngquant: {
+        quality: [0.8, 0.9]
+      },
+      // svg 优化
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox'
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
