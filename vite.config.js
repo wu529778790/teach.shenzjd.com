a29552a24c2fe531fc35a14c5610095a0a040bca
wuwei <wuwei@fadada.com>
2024-04-18 17:13:12 +0800

    fix: 调整打包配置

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,25 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), visualizer({ open: false })],
   resolve: {
     alias: {
       "@": "/src/client",
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vue: ['vue', 'vue-router'],
+          antd: ['ant-design-vue', '@ant-design/icons-vue'],
+          tinymce: ['tinymce', '@tinymce/tinymce-vue'],
+          axios: ['axios'],
+        }
+      }
+    }
+  }
 });
