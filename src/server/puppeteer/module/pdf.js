624f54821923412dd887f7c46e21e449a65e0f7a
wuwei <wuwei@fadada.com>
2024-04-25 10:46:47 +0800

    fix: 修改pdf参数

--- src/server/puppeteer/module/pdf.js
+++ src/server/puppeteer/module/pdf.js
@@ -55,27 +55,27 @@ export default async (req, res) => {
       }
       // 生成pdf
       data = await page.pdf({
-        width: 375,
-        height: 812,
-        displayHeaderFooter: false, // 是否显示页眉和页脚
-        footerTemplate: "", // 页脚模板
-        format: "Letter", // 页面大小
-        headerTemplate: "", // 页眉模板
-        landscape: false, // 是否横向
+        width,
+        height,
+        displayHeaderFooter, // 是否显示页眉和页脚
+        footerTemplate, // 页脚模板
+        format, // 页面大小
+        headerTemplate, // 页眉模板
+        landscape, // 是否横向
         margin: {
-          top: '',
-          bottom: '',
-          left: '',
-          right: '',
+          top: margin.top,
+          bottom: margin.bottom,
+          left: margin.left,
+          right: margin.right,
         },
-        omitBackground: false, // 隐藏默认的白色背景，并允许生成透明的 pdf。
-        outline: false, // 生成大纲
-        pageRanges: "", // 要打印的纸张范围，例如 1-5, 8, 11-13。 空字符串，表示打印所有页面。
-        preferCSSPageSize: false, // 优先使用 CSS 页面大小:为页面中声明的任何 CSS @page 大小提供优先于 width 或 height 或 format 选项中声明的大小
-        printBackground: true, // 打印背景
-        scale: 1, // 页面缩放
-        tagged: false, // 是否打印标签
-        timeout: 30000, // 超时时间
+        omitBackground, // 隐藏默认的白色背景，并允许生成透明的 pdf。
+        outline, // 生成大纲
+        pageRanges, // 要打印的纸张范围，例如 1-5, 8, 11-13。 空字符串，表示打印所有页面。
+        preferCSSPageSize, // 优先使用 CSS 页面大小:为页面中声明的任何 CSS @page 大小提供优先于 width 或 height 或 format 选项中声明的大小
+        printBackground, // 打印背景
+        scale, // 页面缩放
+        tagged, // 是否打印标签
+        timeout, // 超时时间
       })
     } catch (error) {
       throw error;
