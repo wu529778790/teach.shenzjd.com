fef33dd56e3f9f9d9328e0eae8e5f421131c783b
wuwei <wuwei@fadada.com>
2024-05-13 17:30:44 +0800

    feat: 增加当前页面下载

--- src/client/views/wallpaper/components/Wallpaper/imgBox.vue
+++ src/client/views/wallpaper/components/Wallpaper/imgBox.vue
@@ -58,9 +58,12 @@ const handleDownload = (item) => {
         : item.split("x")[1],
     quality: 100,
   });
-  window.open(
-    `http://image.baidu.com/search/down?tn=download&word=download&ie=utf8&fr=detail&url=${url}`
-  );
+  const a = document.createElement("a");
+  a.href = `http://image.baidu.com/search/down?tn=download&word=download&ie=utf8&fr=detail&url=${url}`;
+  a.style = "display: none";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
 };
 </script>
 
