977d3716c2772d60c963e8eb4b462a5e9b926230
wuwei <wuwei@fadada.com>
2024-06-28 16:48:29 +0800

    feat: 引入tdesign

--- src/client/main.js
+++ src/client/main.js
@@ -2,8 +2,12 @@ import { createApp } from "vue";
 import { setupRouter } from "@/router";
 import App from "./App.vue";
 import "./style.css";
+import TDesign from "tdesign-vue-next";
+import "tdesign-vue-next/es/style/index.css";
 
 const app = createApp(App);
+
+app.use(TDesign);
 
 // 配置路由
 setupRouter(app);
